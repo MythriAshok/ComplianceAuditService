f846dcd829b24d082d6e24d4cd7624068341ab0e
MythriAshok <mythriashok.aak@gmail.com>
2018-06-28 21:27:36 +0530

    models

---
+++ ComplianceAuditWeb/Models/OrganizationViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Compliance.DataObject;
+
+namespace ComplianceAuditWeb.Models
+{
+    public class OrganizationViewModel
+    {
+        public Organization organization { get; set; }
+        public Branch branch { get; set; }
+    }
+}
