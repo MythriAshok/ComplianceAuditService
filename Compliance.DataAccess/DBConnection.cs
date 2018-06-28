ff9cca84177bd8ba09ceaf68a3ebf3be5e9eacc6
ojeshwini H P <ojeshwini96@gmail.com>
2018-06-28 21:31:09 +0530

    Added helper classes in DataAccess

---
+++ Compliance.DataAccess/DBConnection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+
+namespace Compliance.DataAccess
+{
+    class DBConnection
+    {
+
+        MySql.Data.MySqlClient.MySqlConnection connection = ""; 
+       
+        
+     
+        
+    }
+}
