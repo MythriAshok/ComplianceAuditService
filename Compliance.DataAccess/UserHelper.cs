21a1eb39c51ffb883ecb7a3a287f87e82b207b26
ojeshwini H P <ojeshwini96@gmail.com>
2018-07-01 22:10:11 +0530

    Modified helper classes

--- Compliance.DataAccess/UserHelper.cs
+++ Compliance.DataAccess/UserHelper.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿#region Code History
+/*CODE HISTORY
+ * ============================================================================================================
+ *  Version No      DATE       Developer Name        Description
+ * ===========================================================================================================
+ *  1.0          28-06-2018    Ojeshwini H P        DataAccess Layer for User DataTable
+ *                                                  insertupdateUser method
+ *                                                  DeleteUser method
+ *                                                  getUser method
+ *  
+ */
+#endregion
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,30 +25,37 @@ namespace Compliance.DataAccess
     public class UserHelper
     {
         MySqlConnection conn = new MySqlConnection();
-        public string createUser(User user)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="user">User object</param>
+        /// <param name="flag"></param>
+        /// <returns></returns>
+        public string insertupdateUser(User user,char flag)
         {
             string result="";
              
                 try
                 {
-                if (user != null)
-                {
+                    if (user != null)
+                    {
                     conn = DBConnection.getconnection();
                     conn.Open();
-                    MySqlCommand cmd = new MySqlCommand("sp_createuser", conn);
+                    MySqlCommand cmd = new MySqlCommand("sp_insertupdateUser", conn);
                     cmd.CommandType = CommandType.StoredProcedure;
-                    cmd.Parameters.AddWithValue("@userPassword", user.UserPassword);
-                    cmd.Parameters.AddWithValue("@FirstName", user.FirstName);
-                    cmd.Parameters.AddWithValue("@MiddleName", user.MiddleName);
-                    cmd.Parameters.AddWithValue("@LastName", user.LastName);
-                    cmd.Parameters.AddWithValue("@EmailId", user.EmailId);
-                    cmd.Parameters.AddWithValue("@ContactNumber", user.ContactNumber);
-                    cmd.Parameters.AddWithValue("@Gender", user.Gender);
-                    cmd.Parameters.AddWithValue("@IsActive", user.IsActive);
+                    cmd.Parameters.AddWithValue("p_flag", flag);
+                    cmd.Parameters.AddWithValue("p_User_ID", user.UserId);
+                    cmd.Parameters.AddWithValue("p_User_Password", user.UserPassword);
+                    cmd.Parameters.AddWithValue("p_First_Name", user.FirstName);
+                    cmd.Parameters.AddWithValue("p_Middle_Name", user.MiddleName);
+                    cmd.Parameters.AddWithValue("p_Last_Name", user.LastName);
+                    cmd.Parameters.AddWithValue("p_Email_ID", user.EmailId);
+                    cmd.Parameters.AddWithValue("p_Contact_Number", user.ContactNumber);
+                    cmd.Parameters.AddWithValue("p_Gender", user.Gender);
+                    cmd.Parameters.AddWithValue("p_Is_Active", user.IsActive);
 
-                    object ires = (cmd.ExecuteScalar());
-                    result = ires.ToString();
-                }
+                    result = Convert.ToString(cmd.ExecuteScalar());
+                    }
                 }
             catch 
             {
@@ -49,26 +69,25 @@ namespace Compliance.DataAccess
             return result;
         }
 
-        public bool DeleteUser(User user)
+        public bool DeleteUser(int userId)
         {
             bool result=false;
-            MySqlConnection conn = new MySqlConnection();
             try
             {
                 conn = DBConnection.getconnection();
                 conn.Open();
                 MySqlCommand cmd = new MySqlCommand("sp_createuser", conn);
                 cmd.CommandType = CommandType.StoredProcedure;
-                cmd.Parameters.AddWithValue("@UserId", user.UserId);
+                cmd.Parameters.AddWithValue("p_User_ID", userId);
                 int res=cmd.ExecuteNonQuery();
                 if(res>0)
                 {
                     result = true;
                 }
             }
-            catch (Exception ex)
+            catch 
             {
-                return false;
+                throw;
             }
             finally
             {
@@ -78,6 +97,29 @@ namespace Compliance.DataAccess
             return result;
         }
 
+        public DataTable getUser(int userId)
+        {
+            DataTable dtUser = new DataTable();
+            try
+            {
+                conn = DBConnection.getconnection();
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("sp_createuser", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("p_User_ID", userId);
+                MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                adapter.Fill(dtUser);
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                conn.Close();
+            }
 
+            return dtUser;
+        }
     }
 }
--- Compliance.DataAccess/UserRolesHelper.cs
+++ Compliance.DataAccess/UserRolesHelper.cs
@@ -3,10 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Compliance.DataObject;
+using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace Compliance.DataAccess
 {
     class UserRolesHelper
     {
+        MySqlConnection conn = new MySqlConnection();
+
+        public DataTable getUserRole(int RoleID)
+        {
+            DataTable dtUser = new DataTable();
+            try
+            {
+                conn = DBConnection.getconnection();
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("sp_getUserGroup", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("p_User_Group_ID", RoleID);
+                MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                adapter.Fill(dtUser);
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return dtUser;
+        }
     }
 }
