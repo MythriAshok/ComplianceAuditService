4a59393c1d45adf29fd444c568e569ee949ed92b
ojeshwini H P <ojeshwini96@gmail.com>
2018-07-03 18:49:06 +0530

    modified user model

---
+++ ComplianceAuditWeb/Models/UserViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Compliance.DataObject;
+
+namespace ComplianceAuditWeb.Models
+{
+    public class UserViewModel
+    {
+       public User User { get; set; }
+       public UserGroup userGroup { get; set; }
+       public UserRoles roles { get; set; } 
+
+    }
+}
