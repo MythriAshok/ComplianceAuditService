53da21f671768d2855c494bfbfef715b54b5f10f
MythriAshok <mythriashok.aak@gmail.com>
2018-07-03 19:05:27 +0530

    view model

--- ComplianceAuditWeb/Controllers/ManageOrganizationController.cs
+++ ComplianceAuditWeb/Controllers/ManageOrganizationController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+//using Compliance.DataObject;
+using ComplianceService;
+using ComplianceAuditWeb.Models;
 
 namespace ComplianceAuditWeb.Controllers
 {
@@ -11,6 +14,18 @@ namespace ComplianceAuditWeb.Controllers
         // GET: ManageOrganization
         public ActionResult Index()
         {
+            return View();
+        }
+        public ActionResult AddGroup()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddGroup(OrganizationViewModel viewModel)
+        {
+            
+            //BranchLocationService1.LocationServiceClient client = new BranchLocationService1.LocationServiceClient();
+           // int id = client.insertBranchLocation(branch);
             return View();
         }
     }
