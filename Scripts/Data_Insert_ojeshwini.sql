5eb876c7b232543696254ff3fede9dcc0d222b0c
ojeshwini H P <ojeshwini96@gmail.com>
2018-07-13 12:10:06 +0530

    Added one more sql file for data insertion

---
+++ Scripts/Data_Insert_ojeshwini.sql
@@ -0,0 +1,28 @@
+INSERT INTO `auditmoduledb`.`tbl_privilege`
+(`Privilege_Name`,
+`Privilege_Type`,
+`Is_Active`)
+VALUES
+("Create","",1);
+
+INSERT INTO `auditmoduledb`.`tbl_privilege`
+(`Privilege_Name`,
+`Privilege_Type`,
+`Is_Active`)
+VALUES
+("Update","",1);
+
+INSERT INTO `auditmoduledb`.`tbl_privilege`
+(`Privilege_Name`,
+`Privilege_Type`,
+`Is_Active`)
+VALUES
+("Read","",1);
+
+INSERT INTO `auditmoduledb`.`tbl_privilege`
+(`Privilege_Name`,
+`Privilege_Type`,
+`Is_Active`)
+VALUES
+("Delete","",1);
+
