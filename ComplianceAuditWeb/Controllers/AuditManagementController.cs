5e25da9defc71f80f315a1e1e8534de074f2d9f5
MythriAshok <mythriashok.aak@gmail.com>
2018-07-20 12:45:16 +0530

    images

--- ComplianceAuditWeb/Controllers/AuditManagementController.cs
+++ ComplianceAuditWeb/Controllers/AuditManagementController.cs
@@ -19,8 +19,9 @@ namespace ComplianceAuditWeb.Controllers
             return View();
         }
 
-        public ActionResult addComplianceAudit (string selectedcompanyid)
+        public ActionResult addComplianceAudit(string selectedcompanyid)
         {
+            selectedcompanyid = "17";
             int ComplianceBrachID = 18;
             int AuditorID = 1;
             ComplianceAudit complianceAudit = new ComplianceAudit();
@@ -32,9 +33,9 @@ namespace ComplianceAuditWeb.Controllers
             DataSet dsCompany = new DataSet();
             dsCompany.ReadXml(new StringReader(strxmlCompany));
 
-            
+
             auditViewModel.MappedCompany = new List<SelectListItem>();
-            
+
             if (dsCompany.Tables.Count > 0)
             {
                 foreach (System.Data.DataRow row in dsCompany.Tables[0].Rows)
@@ -43,13 +44,13 @@ namespace ComplianceAuditWeb.Controllers
                     {
                         auditViewModel.MappedCompany.Add(new SelectListItem() { Text = row["Company_Name"].ToString(), Value = row["Org_Hier_ID"].ToString() });
                     }
-                    }
+                }
             }
 
             string strxmlBranch = auditServiceClient.getAllCompanyBrnachAssignedtoAuditor(AuditorID);
             DataSet dsBranch = new DataSet();
             dsBranch.ReadXml(new StringReader(strxmlBranch));
-       
+
             auditViewModel.MappedBranch = new List<SelectListItem>();
 
             if (dsCompany.Tables.Count > 0)
@@ -58,6 +59,7 @@ namespace ComplianceAuditWeb.Controllers
                 {
                     if (row["Parent_Company_ID"].ToString() == selectedcompanyid)
                     {
+                        
                         auditViewModel.MappedBranch.Add(new SelectListItem() { Text = row["Company_Name"].ToString(), Value = row["Org_Hier_ID"].ToString() });
                     }
                 }
@@ -67,32 +69,38 @@ namespace ComplianceAuditWeb.Controllers
             string strxmlComplianceData = auditServiceClient.getComplianceXref(ComplianceBrachID);
             DataSet dsComplianceXrefData = new DataSet();
             dsComplianceXrefData.ReadXml(new StringReader(strxmlComplianceData));
+            auditViewModel.complianceXrefList = new List<ComplianceXref>();
+            auditViewModel.ComplianceXrefData = new ComplianceXref();
             foreach (System.Data.DataRow row in dsComplianceXrefData.Tables[0].Rows)
             {
-                AuditViewModel auditViewModelforacts = new AuditViewModel
 
-                {
-                     
-                
-                    OrganizationID = Convert.ToInt32(row["Org_Hier_ID"]),
-                    CompanyName = row["Company_Name"].ToString(),
-                    IsActive = Convert.ToBoolean(Convert.ToInt32(row["Is_Active"]))
+                auditViewModel.complianceXrefList.Add(new ComplianceXref() {
+                    Compliance_Xref_ID = Convert.ToInt32(row["Compliance_Xref_ID"]),
+                    Compliance_Parent_ID = Convert.ToInt32(row["Compliance_Parent_ID"]),
+                    Compliance_Title = Convert.ToString(row["Compliance_Title"]),
+                    Comp_Category= Convert.ToString(row["Comp_Category"])
+                });
 
-               
+                //auditViewModel.ComplianceXrefData.Compliance_Xref_ID = Convert.ToInt32(row["Compliance_Xref_ID"]);
+                //auditViewModel.ComplianceXrefData.Compliance_Parent_ID = Convert.ToInt32(row["Compliance_Parent_ID"]);
+                //auditViewModel.ComplianceXrefData.Compliance_Title = Convert.ToString(row["Compliance_Title"]);
+                //auditViewModel.ComplianceXrefData.Comp_Category = Convert.ToString(row["Comp_Category"];
 
 
-            };
-                auditViewModelsList.Add(auditViewModelforacts   );
+
                 auditViewModelsList.Add(auditViewModel);
-            }
-            return View(auditViewModelsList);
+                ViewBag.List = auditViewModelsList;
+                }
+            
+            return View("_complianceAuditing", ViewBag.List);
+
         }
 
         [HttpPost]
         public ActionResult addComplianceAudit(List<AuditViewModel> auditViewModelsList)
         {
             AuditService.AuditServiceClient auditServiceClient = new AuditService.AuditServiceClient();
-            auditServiceClient.insertComplianceAudit(auditViewModelsList);
+           // auditServiceClient.insertComplianceAudit(auditViewModelsList);
             return View();
         }
     }
--- ComplianceAuditWeb/Models/AuditViewModel.cs
+++ ComplianceAuditWeb/Models/AuditViewModel.cs
@@ -10,10 +10,10 @@ namespace ComplianceAuditWeb.Models
     public class AuditViewModel
     {
         public ComplianceXref ComplianceXrefData { get; set; }
-        public IEnumerable<ComplianceXref> complianceXrefList { get; set; }
+        public List<ComplianceXref> complianceXrefList { get; set; }
         public ComplianceOptions ComplianceOptions { get; set; }
         public ComplianceAudit complianceAudit { get; set; }
-        public IEnumerable<ComplianceAudit> complianceAuditList { get; set; }
+        public List<ComplianceAudit> complianceAuditList { get; set; }
 
         public List<SelectListItem> MappedCompany { get; set; }
         public List<SelectListItem> MappedBranch { get; set; }
